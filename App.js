cdd11f2f32fbf9430fee06b20c6c9d05f0866875
vidhi vats <vidhi.vats@tothenew.com>
2021-10-05 14:07:39 +0530

    Functionalcomponentquestion

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Child from './Src/Components/Child';
+import Parent from './Src/Components/Parent';
+import Test from './Src/Components/Test';
+import Parentfunctionalcomponent from './Src/Components/Parentfunctionalcomponent'
 
  function App() {
   return (
     
-      <Child/>
+      <Parentfunctionalcomponent/>
 
   );
 }
---
+++ Src/Components/Parentfunctionalcomponent.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import {View,Text, Button}from "react-native";
+
+const Parentfunctionalcomponent = ()=>
+{
+
+    const [value,newvalue] = useState(0);
+    
+    return(
+        <View style = {{paddingTop:30}}>
+            <Text>value is {value}</Text>
+            <Button onPress={()=>newvalue(value+1)} title="IncrementCounter"></Button>
+            <ChildFunctionalComponent countervalue = {value}/>
+        </View>
+    );
+}
+
+
+const ChildFunctionalComponent = (Props) =>
+{
+    return(
+           <View style = {{paddingTop:30}}>
+               <Text>{Props.value}</Text>
+           </View>
+    );
+}
+export default Parentfunctionalcomponent;
